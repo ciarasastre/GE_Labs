2c1b8206e25184cfaa65170c5e78ee3aa21c0277
Ciara Sastre <sastreciara@hotmail.com>
2018-09-22 18:36:28 +0100

    Unity lab1 commit

---
+++ Lab1_Tank/Assets/Scripts/bulletScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletScript : MonoBehaviour {
+
+    public GameObject bullets;
+    public float bull_xPos = 0 / 5;
+    public float bull_yPos = 1;
+    public float bull_zPos = 2;
+
+    private void Start()
+    {
+        //Instantiate(bullets, transform.position, transform.rotation);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Now get the bullets working
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullets, new Vector3(bull_xPos, bull_yPos, bull_zPos), Quaternion.Euler(0, 180, 0));
+        }
+
+    }
+
+    void Fire_Bullet()
+    { 
+
+
+    }
+
+}
---
+++ Lab1_Tank/Assets/Scripts/movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    private Rigidbody tankBody;
+
+    int move_speed = 5;
+    int rotate_speed = 15;
+
+    // Use this for initialization
+    void Start () {
+
+        tankBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+
+        //Move tank in accordance to button pressing
+        if (Input.GetKey("up"))
+        {
+            //Tank_Body.Translate(Vector3.left, Time.deltaTime, Space.World););
+            tankBody.transform.Translate(0, 0, move_speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            tankBody.transform.Translate(0, 0, -move_speed * Time.deltaTime);
+        }
+
+        //Move tank by twisting left and right
+        if (Input.GetKey("left"))
+        {
+            tankBody.transform.Rotate(0, -rotate_speed * Time.deltaTime, 0);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            tankBody.transform.Rotate(0, rotate_speed * Time.deltaTime, 0);
+        }
+
+    }
+}
